af0db8b8e0090a81f499ec8ce27be6cc5f02cbfe
Lev Lybin <github@devtrue.net>
2013-10-07 16:59:27 -0700

    REF

--- grab.py
+++ grab.py
@@ -22,27 +22,27 @@ def rnd():
 def chk():
     while True:
         try:
-            path = u'http://' + host + u"/c/" + rnd()
+            path = 'http://%s/%s/%s' % (host, random.choice(string.ascii_lowercase), rnd())
             status = requests.head(path, timeout=10).status_code
+            print status
             if status == 200:
-                content = requests.get(path, timeout=10).content
+                content = requests.get(path).content
                 if content.find('<section class="image">') > 0:
                     page = BeautifulSoup(''.join(content))
                     imgalt = page.findAll('section', {'class': "image"})[0].img['alt']
-                    print imgalt
+                    print '%s - %s' % (path, imgalt)
                     imgsrc = page.findAll('section', {'class': "image"})[0].img['src']
                     o = open(dir + unicodedata.normalize('NFKD', imgalt.replace(":", "_").replace("\\", "_").replace("/", "_")).encode('utf-8', 'ignore'), 'wb')
                     o.write(requests.get(imgsrc).content)
                     o.close()
                 elif content.find('<section class="text note">') > 0:
-                    print path + ' - text note'
+                    print '%s - text note' % path
                 else:
-                    print path + ' - file'
-        except BaseException:
-            pass
-
+                    print '%s - file' % path
+        except BaseException as e:
+            print e
 
 if __name__ == "__main__":
-    for i in range(30):
+    for i in range(15):
         t = Thread(target=chk)
         t.start()
